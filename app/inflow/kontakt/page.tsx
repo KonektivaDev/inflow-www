6c5a118fc6d781eea10a7ac7fec19c1f3ad1617e
Marek Tomaszewski <dev.konektiva@gmail.com>
2025-03-20 09:37:22 +0100

    kontakt page update

--- app/inflow/kontakt/page.tsx
+++ app/inflow/kontakt/page.tsx
@@ -12,13 +12,27 @@ export default function ContactPage() {
         poniższych sposobów.
       </p>
       <div className="flex flex-col items-start justify-start gap-5">
+        <div className="flex flex-col items-start justify-start gap-2">
+          <span className="text-lg font-bold">Konektiva S.C.</span>
+          <div className="flex items-center justify-start gap-2">
+            <span className="text-sm text-muted-foreground">
+              NIP: 5214094857
+            </span>
+            <span className="text-sm text-muted-foreground">
+              REGON: 540041707
+            </span>
+          </div>
+        </div>
         <div className="flex items-center gap-2">
           <MapPinCheckInsideIcon className="w-10 h-10 text-primary" />
           <span>ul. Świeradowska 47, 02-662 Warszawa</span>
         </div>
-        <Link href="mailto:info@inflow.pl" className="flex items-center gap-2">
+        <Link
+          href="mailto:info@konektiva.pl"
+          className="flex items-center gap-2"
+        >
           <Mail className="w-10 h-10 text-primary" />
-          <span>kontakt@inflow.pl</span>
+          <span>info@konektiva.pl</span>
         </Link>
       </div>
     </div>
